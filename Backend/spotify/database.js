8705cfd520ba42f5d99c2f53e2c5ea27465000d2
HarshitPandey251 <harshitpandey251@gmail.com>
2022-06-09 10:27:13 +0530

    commit

--- Backend/spotify/database.js
+++ Backend/spotify/database.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 const connection = mysql.createPool({
   connectionLimit: 100,
-  host: "database-2-instance-1.cixcceofglai.ap-south-1.rds.amazonaws.com",
-  user: "admin",
-  password: "Deltax1234",
-  database: "deltax",
+  host: "",
+  user: "",
+  password: "",
+  database: "",
 });
 
 module.exports = connection;
